97a5f5d0c2e23717d874eb0ea8ebe3237e7aab0b
Rafli <raflisatriandi@outlook.co.id>
2021-01-19 14:39:00 +0700

    changes on app.js and ejs addedd

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 let express = require("express");
 let app = express();
+let ejs = require("ejs");
+let port = process.env.PORT || 8000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.render("index", {
+        data: {
+            makanan: "nasi",
+            harga: 10000
+        }
+    });
 });
 
+app.get("/:paramId", (req, res) => {
+    let key;
+    if(req.params.paramId === "help"){
+        key = "Get some help!";
+    }else{
+        key = req.params.paramId + " is the key";
+    }
+    res.send(key);
+});
 
-
-app.listen(8000, () => {
-    console.log("Listening at port 8000");
+app.listen(port, () => {
+    console.log("Listening at port "+port);
 });
